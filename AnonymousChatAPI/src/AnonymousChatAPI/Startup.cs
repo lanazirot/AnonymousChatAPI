0359b222e3e5194e89cc34a3bd09e1ec1b5591e7
Alan Peña Ortiz <alancin7@live.com.mx>
2023-05-07 23:22:39 -0500

    Add project files.

---
+++ AnonymousChatAPI/src/AnonymousChatAPI/Startup.cs
@@ -0,0 +1,26 @@
+﻿namespace AnonymousChatAPI {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddControllers();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            if (env.IsDevelopment()) {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.UseAuthentication();
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();
+                endpoints.MapGet("/", async context => {
+                    await context.Response.WriteAsync("Entry point not found for AnonymousChatAPI");
+                });
+            });
+        }
+    }
+}
